703ca71e3d77c852928f16a14861159e235bfecf
aaryanfar <50172756+aaryanfar@users.noreply.github.com>
2019-07-20 22:48:49 +0300

    Add files via upload

---
+++ Research_files/Research.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20190720
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Research_files/ResearchMoz.css')
+adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');fixupIECSS3Opacity('id2');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
