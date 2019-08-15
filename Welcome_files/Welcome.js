7e8b58f9bcf0ab44de3f445e2c568f577102b933
aaryanfar <50172756+aaryanfar@users.noreply.github.com>
2019-08-15 21:13:54 +0300

    Delete Welcome.js

--- Welcome_files/Welcome.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20190508
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');fixupIECSS3Opacity('id3');fixupIECSS3Opacity('id4');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
